0cf3dadd0372c6167517e0e9d203a9ef758bf81d
Adriana Elizabeth <adrianaelizabeth24@users.noreply.github.com>
2016-02-19 03:34:03 -0600

    Log in
    login funcional

--- Controller/Login.php
+++ Controller/Login.php
@@ -5,3 +5,15 @@
  * Date: 19/02/2016
  * Time: 03:14 AM
  */
+
+include "../Model/queryLogin.php";
+$email = $_GET["email"];
+$password = $_GET["password"];
+
+if($email != ""|| $password != "")
+{
+        entrar($email,$password);
+
+}
+
+?>
--- Model/queryLogin.php
+++ Model/queryLogin.php
@@ -6,28 +6,29 @@
  * Time: 03:14 AM
  */
 
-$email = $_GET["email"];
-$password = $_GET["password"];
+function entrar($email,$password)
+{
 // Create
-$conn = new mysqli('localhost', 'root','','osham');
+    $conn = new mysqli('localhost', 'root', '', 'socialnetwork');
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-$result=$conn->query("select * from users where email = '$email' ");
-$row = $result->fetch_assoc();
-if (empty($row["email"])) {
-    echo '<h1>Usuario o password incorrectos, vuelva a intenerarlo</h1>';
-} else {
-    if ($row["password"] != $password) {
-        echo '<h1>Usuario o password incorrectos, vuelva a intenerarlo</h1>';
-    } else {
-        session_start();
-        $_SESSION['isLoggedIn'] = true;
-        header("location:../View/.html");
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
     }
+    $result = $conn->query("select * from account where email = '$email'");
+    $row = $result->fetch_assoc();
+    if (empty($row["email"])) {
+        echo '<h1>NO existe</h1>';
+    } else {
+        if ($row["password"] != $password) {
+            echo '<h1>Usuario o password incorrectos, vuelva a intenerarlo</h1>';
+        } else {
+            session_start();
+            $_SESSION['isLoggedIn'] = true;
+            header("Location: ../View/home.php");
+        }
+    }
+    $result->free();
+    $conn->close();
 }
-$result->free();
-$conn->close();
 
 ?>
